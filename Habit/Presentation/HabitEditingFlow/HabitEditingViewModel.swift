c5bb88280956b2db261cb0448e06a206eb9d60f7
Andrei Krotov <akrotoff95@gmail.com>
2023-05-20 16:01:16 +0300

    post commit with view model

---
+++ Habit/Presentation/HabitEditingFlow/HabitEditingViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  HabitEditingViewModel.swift
+//  Habit
+//
+//  Created by Andrei Krotov on 20/05/2023.
+//
+
+import Foundation
+
+final class HabitEditingViewModel {
+
+    // MARK: - Internal types
+
+    enum RouteKind {
+        case close
+    }
+
+    // MARK: - Private types
+
+    // MARK: - Internal properties
+
+    var model: UserHabit
+
+    // MARK: - Private properties
+
+    private let userHabitsService: UserHabitsServiceType
+
+    private let routeListener: ((RouteKind) -> Void)
+
+    // MARK: - Init
+
+    init(
+        userHabitsService: UserHabitsServiceType,
+        initialHabit: UserHabit?,
+        routeListener: @escaping ((RouteKind) -> Void)
+    ) {
+        model = initialHabit ?? .makeHabit()
+        self.userHabitsService = userHabitsService
+        self.routeListener = { event in DispatchQueue.main.async { routeListener(event) } }
+    }
+
+    // MARK: - Internal methods
+
+    func tappedClose() {
+        routeListener(.close)
+    }
+
+    func updateName(_ name: String) {
+        model.name = name
+        userHabitsService.updateHabit(model)
+    }
+
+    // MARK: - Private methods
+}
